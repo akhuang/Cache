6ae435aaff1d11b6edf48a83f451d3e98c736c36
fy24h <fy24h.huang@gmail.com>
2014-10-30 16:43:13 +0800

    init

---
+++ Zing.Core/ICacheManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zing.Core
+{
+    public interface ICacheManager
+    {
+        T Get<T>(Func<T> func);
+    }
+}
---
+++ src/CacheModule/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CacheModule.MvcApplication" Language="C#" %>
