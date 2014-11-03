89c4a44583816627a195bb782b0a5d3f03d6913c
fy24h <fy24h.huang@gmail.com>
2014-11-03 16:55:14 +0800

    update

---
+++ Zing.Core/Token.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zing.Core
+{
+    public class Token : IToken
+    {
+        public IDictionary<string, IToken> Tokens;
+        public Token()
+        {
+            IsCurrent = true;
+            Tokens = new Dictionary<string, IToken>();
+        }
+
+
+
+        public bool IsCurrent
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Trigger()
+        {
+            IsCurrent = false;
+        }
+    }
+}
