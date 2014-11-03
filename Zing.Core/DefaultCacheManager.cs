7c1825ab84a261e8304bbca38d3c39dd249f200d
fy24h <fy24h.huang@gmail.com>
2014-11-03 16:49:33 +0800

    add UnitTest

---
+++ Zing.Core/DefaultCacheManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zing.Core
+{
+    public class DefaultCacheManager : ICacheManager
+    {
+        public T Get<T>(Func<T> func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/UnitTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Autofac;
+using Zing.Core;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IContainer Container;
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DefaultCacheManager>().As<ICacheManager>();
+            Container = builder.Build();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            ICacheManager cacheManager = Container.Resolve<ICacheManager>();
+
+            cacheManager.Get<RegionEntity>(() =>
+            {
+                return new RegionEntity();
+            });
+        }
+    }
+}
+
